8a4fa4d6c225352547d8c8a8a3213bb37689169d
Upek Hansaja <upekhansajabk@gmail.com>
2024-03-28 15:29:33 +0530

    Crash Course Lesson #1 to #4

---
+++ index.js
@@ -0,0 +1,17 @@
+var age = 19;
+var firstName = 'Upek';
+var isFictional;
+age = 20;
+firstName = 'UpekHansaja';
+isFictional = true;
+var planet = 'Earth';
+var moons = 1;
+var isLarge = false;
+planet = 'Jupitor';
+moons = 79;
+isLarge = true;
+//  null & undefined
+var something;
+var anotherThing;
+something = null;
+anotherThing = undefined;
